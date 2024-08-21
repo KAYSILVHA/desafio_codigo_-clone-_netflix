109c425824112d04c9a506e988d473ff8a18aef1
KAYSILVHA <kaysilvha@gmail.com>
2024-08-20 21:17:03 -0300

    testes

--- src/Components/Pages/Login/Login.jsx
+++ src/Components/Pages/Login/Login.jsx
@@ -4,6 +4,8 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Button, Container, Input } from 'reactstrap';
 
+import "./assets/style/style.scss"
+
 
 function Login() {
   const [email, setEmail] = useState('');
@@ -20,21 +22,25 @@ function Login() {
   };
 
   return (
-    <Container className='container-fluid'>
-      <h2>Login</h2>
-      <Input
-        type="email"
-        placeholder="Email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <Input
-        type="password"
-        placeholder="Senha"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <Button onClick={handleLogin}>Entrar</Button>
+    <Container className='container-bg d-flex align-items-center justify-content-center'>
+      <div className='d-flex flex-column container-data'>
+        <h2>Entrar</h2>
+        <Input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          className='mb-3'
+        />
+        <Input
+          type="password"
+          placeholder="Senha"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          className='mb-3'
+        />
+        <Button onClick={handleLogin}>Entrar</Button>
+      </div>
     </Container>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import Home from './Home.jsx'
-import AppRoutes from './Components/Routes/Route.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import AppRoutes from './Components/Routes/Route.jsx';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
